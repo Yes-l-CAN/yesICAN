992e38694a89ee12c7c1ee03b5575a64c4dffdf5
jisookim <jisookim@student.42seoul.kr>
2023-01-16 16:34:39 +0900

    added utility file

---
+++ Utility.cpp
@@ -0,0 +1,26 @@
+#include "../Utility.hpp"
+
+std::vector<std::string> Utility::splitArr(std::string& str)
+{
+
+    std::vector<std::string> v;
+    size_t                   position = 0;
+    size_t                   start = 0;
+
+    std::string temp;
+    while ((position = str.find(" ", position)) != std::string::npos)
+    {
+        // std::cout << "start : " << start << std::endl;
+        // std::cout << "pos : " << position << std::endl;
+        temp = "";
+        for (size_t i = start; i < position; i++)
+        {
+            temp += str[i];
+        }
+        v.push_back(temp);
+        position += 1;
+        start = position;
+    }
+
+    return (v);
+}
---
+++ ft_irc/include/Utility.hpp
@@ -0,0 +1,11 @@
+#ifndef UTILITY_HPP
+#define UTILITY_HPP
+
+class Utility{
+    private:
+
+    public:
+        std::vector<std::string> splitArr(std::string& str);
+}
+
+#endif
